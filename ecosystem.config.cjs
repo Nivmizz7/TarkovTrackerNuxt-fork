789774776619c31b1a40e32bf2ef730c1d55ec3d
Niv <148060940+Nivmizz7@users.noreply.github.com>
2025-12-17 15:41:44 +0100

    Update ecosystem.config.cjs
    Signed-off-by: Niv <148060940+Nivmizz7@users.noreply.github.com>

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,11 +1,15 @@
 module.exports = {
   apps: [
     {
-      name: "TarkovTracker-nuxt",
+      name: "tarkovtracker-nuxt",
       cwd: __dirname,
       script: "/bin/bash",
       args: "-lc 'npm run dev'",
       autorestart: true,
+      watch: false,
+      env: {
+        NODE_ENV: "production"
+      }
     }
   ]
 }
