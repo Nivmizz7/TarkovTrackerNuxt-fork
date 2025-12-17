00ce9c3251daa2390507f17684727c876d0cfa7b
Niv <148060940+Nivmizz7@users.noreply.github.com>
2025-12-17 15:51:07 +0100

    Rename app and set PORT and NUXT_PORT
    Updated application name and added port configurations.
    
    Signed-off-by: Niv <148060940+Nivmizz7@users.noreply.github.com>

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,14 +1,16 @@
 module.exports = {
   apps: [
     {
-      name: "TarkovTrackerNuxt-fork",
+      name: "tarkovtracker-nuxt",
       cwd: __dirname,
       script: "/bin/bash",
       args: "-lc 'npm run dev'",
       autorestart: true,
       watch: false,
       env: {
-        NODE_ENV: "production"
+        NODE_ENV: "production",
+        PORT: 3156,
+        NUXT_PORT: 3156
       }
     }
   ]
